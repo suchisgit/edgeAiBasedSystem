14320d50a9ed95d1f922861b5b2327d7066f214d
suchisgit <suchandranath.bajjuri@sjsu.edu>
2023-11-06 08:07:49 -0800

    added post API for seding dockerhub image

--- back-end/main.go
+++ back-end/main.go
@@ -7,15 +7,34 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type DockerImage struct {
+	DockerImage string `json:"dockerImage"`
+}
+
 func main() {
-	fmt.Print("Hello world")
+	fmt.Print("Hello GO back-end")
 
 	app := fiber.New()
+
+	// dockerImage := DockerImage{}
 
 	//health check endpoint
 	app.Get("/healthCheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	//post
+	app.Post("/dockerImageName", func(c *fiber.Ctx) error {
+		var req DockerImage
+		// dockerImage := &DockerImage{}
+
+		if err := c.BodyParser(&req); err != nil {
+			return err
+		}
+
+		dockerImage := req.DockerImage
+		fmt.Print(dockerImage)
+		return c.JSON(dockerImage)
+	})
+	log.Fatal(app.Listen(":4001"))
 }
